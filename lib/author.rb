118144f42619967e3e5bd85059bfadf07607238c
Tracy Ngo <tngo1206@gmail.com>
2019-03-20 03:37:37 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+require 'post.rb'
+
+class Author
+
+@@post_count = 0
+
+attr_accessor :name, :posts
+  
+  def initialize(name = nil)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+
+end
